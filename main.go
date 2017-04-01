57c51e95436e373591c8402b20f0531254221499
jingx <1270858488@qq.com>
2017-04-01 18:01:41 +0800

    返回首页

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io/ioutil"
 )
 
 func main()  {
 	fmt.Println("main")
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", index)
 	http.ListenAndServe("localhost:8080", nil)
 }
 
-func handler(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintln(w, "hello, word")
+func index(w http.ResponseWriter, r *http.Request)  {
+	bytes, err := ioutil.ReadFile("webapp/index.html")
+	if (err != nil) {
+		fmt.Fprintln(w, err)
+		fmt.Println(err)
+	}
+	fmt.Fprintln(w, string(bytes))
 }
